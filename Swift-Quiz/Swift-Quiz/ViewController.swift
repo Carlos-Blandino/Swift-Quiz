6b6bbeeefef745292cbfa55d56cdb190303c366f
Carlos Blandino <cablandino66@gmail.com>
2020-05-19 19:50:59 -0700

    working on main functionality, answerButtonPressed, checkAnswers,and updateQuestion

--- Swift-Quiz/Swift-Quiz/ViewController.swift
+++ Swift-Quiz/Swift-Quiz/ViewController.swift
@@ -9,21 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
-    
+   
+    var correctAnswer = ""
+    var quiz = [
+        ["Four + Two is equal to Six","True"],
+        ["Five - Three is greater than One","True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //at start up I needed to set the progressbar to starting position
         progressBar.progress = 0
-    }
-
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        //I need the first random question to appear
+        updateQuestion()
     }
     
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        //temporarily setup some local variable to allow checking of
+        //correct answer
+        
+        checkAnswer(currentTitle: sender.currentTitle!)
+        
+        //bringing up the next question after user answers
+        updateQuestion()
+    }
+    
+    func checkAnswer(currentTitle: String) {
+        
+        //this is buttonPressed helper function
+        //that will verify
+        //if correct button was pressed
+        
+        let userAnswer = currentTitle
+        let actualAnswer = correctAnswer
+        
+        if userAnswer == actualAnswer {
+            print("Correct")
+        } else {
+            print("Wrong")
+        }
+    }
+    
+    func updateQuestion() {
+        //since the quiz is being reduce by the current
+        //question we need to first check for an empty
+        //array
+        if quiz.isEmpty {
+            questionLabel.text = "Quiz is done"
+        //we randomly select the next question if the
+        //array is not empty and assign its answer to the
+        //temp global variable correctAnswer
+        //and afterwards remove element form the array
+        //so we don't get a duplicate or predictable question
+        } else {
+            let strElementArray = quiz.randomElement()
+            let index = quiz.firstIndex(of: strElementArray!)
+            
+            questionLabel.text = quiz[index!][0]
+            correctAnswer = quiz[index!][1]
+            quiz.remove(at: index!)
+            
+        }
+        
+        
+        
+        
+    }
     
 }
 
