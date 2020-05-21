d487fbacd70a812e16e22191c9e3f74092534af9
Carlos Blandino <cablandino66@gmail.com>
2020-05-21 09:55:23 -0700

    creating button animated response

--- Swift-Quiz/Swift-Quiz/ViewController.swift
+++ Swift-Quiz/Swift-Quiz/ViewController.swift
@@ -21,11 +21,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //change the style of the buttons programmatically
+        changeButton(button: trueButton)
+        changeButton(button: falseButton)
+        //get the questions ready
         addQuestions()
         //at start up I needed to set the progressbar to starting position
         progressBar.progress = 0
-        //I need the first random question to appear
+        //need the first random question to appear
         updateQuestion()
+    }
+    
+    func changeButton(button: UIButton) {
+        button.layer.borderColor = CGColor.init(srgbRed: 200, green: 200, blue: 200, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 3
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
@@ -33,10 +43,10 @@ class ViewController: UIViewController {
         //temporarily setup some local variable to allow checking of
         //correct answer
         
-        checkAnswer(currentTitle: sender.currentTitle!)
+        checkAnswer(currentTitle: sender.currentTitle!, button:  sender)
         
         //bringing up the next question after user answers
-        updateQuestion()
+        //updateQuestion()
     }
     
     func addQuestions() {
@@ -45,7 +55,7 @@ class ViewController: UIViewController {
         quiz.addQuestion(question: "Three + Eight is less than Ten", ans: "False")
     }
     
-    func checkAnswer(currentTitle: String) {
+    func checkAnswer(currentTitle: String, button: UIButton) {
         
         //this is buttonPressed helper function
         //that will verify
@@ -56,29 +66,42 @@ class ViewController: UIViewController {
         
         if userAnswer == actualAnswer {
             print("Correct")
+            button.backgroundColor = .systemGreen
+            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateQuestion), userInfo: nil, repeats: false)
         } else {
+            button.backgroundColor = .systemRed
+            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateQuestion), userInfo: nil, repeats: false)
             print("Wrong")
         }
     }
 
-    func updateQuestion() {
+    @objc func updateQuestion() {
+        
+        //reset the buttons to original state
+        trueButton.backgroundColor = .clear
+        falseButton.backgroundColor = .clear
+        
         //since the quiz is being reduce by the current
         //question we need to first check for an empty
         //array
         
         if quiz.questions.isEmpty {
-    
+        //viewDidLoad resets the quiz
         viewDidLoad()
-        //we randomly select the next question if the
-        //array is not empty and assign its answer to the
-        //temp global variable correctAnswer
-        //and afterwards remove element form the array
-        //so we don't get a duplicate or predictable question
+        
+        
         } else {
+            //we randomly select the next question if the
             let strElementKey = quiz.questions.randomElement()
             let index = strElementKey!.key
             questionLabel.text = index
+            
+            //array is not empty and assign its answer to the
+            //temp global variable correctAnswer
             correctAnswer = strElementKey?.value ?? ""
+            
+            //so we don't get a duplicate or predictable question
+            //and afterwards remove element form the array
             quiz.questions.remove(at: quiz.questions.index(forKey: index)!)
         }
     }
